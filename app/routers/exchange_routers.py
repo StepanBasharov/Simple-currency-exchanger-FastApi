379a84499fb0fb8a29309da19dafe9d8dfef37ad
stepan.basharov <stepan.basharov@simbirsoft.com>
2023-12-03 05:01:36 +0300

    draft

--- app/routers/exchange_routers.py
+++ app/routers/exchange_routers.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from app.schemas.exchange_schemas import ConvertRequest
 from app.models.exchange_models import ExchangeCurrency
@@ -10,13 +11,10 @@ exchange_router = APIRouter(prefix="/exchange")
 
 @exchange_router.get("/last_db_update")
 async def get_last_db_update(session: AsyncSession = Depends(get_session)):
-    new_currency = ExchangeCurrency(
-        currency_name="Рубль",
-        currency_symbol="RUB",
-        currency_price=100.4
-    )
-    session.add(new_currency)
-    await session.commit()
+    data = select(ExchangeCurrency)
+    data = await session.execute(data)
+    data = data.scalars().all()
+    print(data)
     return "OK"
 
 
--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from fastapi import FastAPI
 import uvicorn
 
 from app.routers import api
+from app.jobs.update_job import celery_app
 from settings.config import ConfigService
 
 app = FastAPI()
 
 app.include_router(api)
+
+# @app.on_event("startup")
+# async def start_up():
+#    celery_app.send_task("jobs.update_job.update_currency")
 
 if __name__ == "__main__":
     uvicorn.run(
